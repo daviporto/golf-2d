4e4bc7bc57f12d6e6dbbe8212f4e4c220ed6d10a
aliciachaves <103082430+aliciachaves@users.noreply.github.com>
2022-12-01 18:54:33 +0000

    Add files via upload

---
+++ Level2.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Level.h"
+class Level2 :
+    public Level
+{
+private:
+    Obstacle* obstacle;
+    Obstacle* obstacle_1;
+    Obstacle* obstacle_2;
+    Obstacle* obstacle_3;
+public:
+    Level2();
+    void Mover_Ball(double Var_x, double Var_y, bool* run) override;
+    ~Level2();
+};
+
